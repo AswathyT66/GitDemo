7864b75251608a6b9e4090cd69bf03d412060905
AswathyT66 <aswathyachu25295@gmail.com>
2021-03-27 19:26:26 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void autmaion()
+	{
+		System.out.println("Automation");
+	}
+	
+	
+	@Test
+	public void Manual()
+	{
+		System.out.println("Manual");
+	}
+
+}
